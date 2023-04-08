39f91c3af482cd68c2f00651be7d8986c9f40190
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-04-08 18:27:33 +0100

    first commit

---
+++ src/test/java/com/amyojiakor/NotificationMicroService/NotificationMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyojiakor.NotificationMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
