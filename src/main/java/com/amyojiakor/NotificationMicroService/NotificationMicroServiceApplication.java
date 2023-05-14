d585f93565827d5f6de1b87399072ca6631b4e32
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-05-14 22:14:34 +0100

    Add component scan for package 'com.amyojiakor.NotificationMicroService'

--- src/main/java/com/amyojiakor/NotificationMicroService/NotificationMicroServiceApplication.java
+++ src/main/java/com/amyojiakor/NotificationMicroService/NotificationMicroServiceApplication.java
@@ -2,8 +2,10 @@ package com.amyojiakor.NotificationMicroService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.amyojiakor.NotificationMicroService")
 public class NotificationMicroServiceApplication {
 
 	public static void main(String[] args) {
